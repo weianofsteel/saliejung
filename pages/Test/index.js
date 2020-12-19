94fa730ba407f87c855dfd75ba50b36ab9e31a9a
weianofsteel <skywalker0631@gmail.com>
2020-12-19 14:22:10 +0800

    fix import error

--- pages/Test/index.js
+++ pages/Test/index.js
@@ -1,7 +1,3 @@
-// import Carousel2 from '../../component/Public/Carousel2.js';
-import process01 from '../../public/process01.png';
-import process02 from '../../public/process02.png';
-import process03 from '../../public/process03.png';
 import photo3 from '../../public/3.jpeg';
 import photo4 from '../../public/4.jpeg';
 
