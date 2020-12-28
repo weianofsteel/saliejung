5ed3842b90c6b0938409c059ca3a506f8cb2091b
Weian Wang <weian.wang@wakeasp.com.tw>
2020-12-28 18:45:03 +0800

    agoda-design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0441443e9393ec8d2e4d";
+/******/ 	var hotCurrentHash = "c6cb2aa40070dd323bc0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- component/Work/AgodaDesign.js
+++ component/Work/AgodaDesign.js
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import styles from '../../css/Work.module.css';
 import agoda from '../../public/agoda-logo.png';
 import Hidden from '@material-ui/core/Hidden';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export const AgodaDesign = () => {
     return(
@@ -47,6 +48,62 @@ export const AgodaDesign = () => {
                     <h2 className={styles.title4}>§</h2>
                 </Grid>     
                 <Grid item xs={false} md={3}></Grid>
+            </Grid>
+
+            <Grid container style={{marginTop:'5%'}}>
+                <Grid item xs={false} md={3}></Grid>
+                <Grid item xs={12} md={6}>
+                    <p className={styles.article}>
+                        Joined the Agoda Design team is not only a career path but 
+                        also a life-changing experience for me. I want to share 
+                        some highlighting stories on this page.
+                        <br/>
+                    </p>
+                </Grid>     
+                <Grid item xs={false} md={3}></Grid>
+            </Grid>
+
+            <Grid container style={{marginTop:'5%'}}>
+                <Grid item xs={false} md={3}></Grid>
+                <Grid item xs={12} md={6}>
+                    <Grid container spacing={5} className={styles.content}>
+                        <Grid item xs={4}>
+                            <span className={styles.contentTitle}>Timeline</span>
+                            <br/>
+                            <span className={styles.contentDetail}>
+                                June 2019 to present
+                            </span>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <span className={styles.contentTitle}>Team I joined</span>
+                            <br/>
+                            <span className={styles.contentDetail}>
+                                Flights, Product Marketing, SEO, Pricing
+                            </span>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <span className={styles.contentTitle}>My role</span>
+                            <br/>
+                            <span className={styles.contentDetail}>
+                                Competitive Analysis, User interview, Design Research, UI/UX Design (Web & App)
+                            </span>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={false} md={3}></Grid>
+            </Grid>
+
+            <Grid container style={{marginTop:'8%'}} className={styles.scrollDown}>
+                <Grid item xs={2} md={4}></Grid>
+                <Grid item xs={8} md={4} style={{textAlign:'center'}}>
+                    <span>
+                        LEARN MORE
+                    </span>
+                </Grid>
+                <Grid item xs={2} md={4}></Grid>
+                <Grid item xs={12} style={{textAlign:'center'}}>
+                    <ExpandMoreIcon/>
+                </Grid>
             </Grid>
 
         </React.Fragment>
