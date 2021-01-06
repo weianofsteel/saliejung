365f4d0b487bab9372496cb25bf32fb27d22db54
weianofsteel <skywalker0631@gmail.com>
2021-01-06 22:48:22 +0800

    switch img

---
+++ .next/static/webpack/pages/Work/Agoda-design.27379b6e3df03948c8bf.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate_N_E("pages/Work/Agoda-design",{
+
+/***/ "./public/agoda-design/design08.png":
+/*!******************************************!*\
+  !*** ./public/agoda-design/design08.png ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/design08-e41922888ba7b92903f4f2bb551bdea3.png";
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcHVibGljL2Fnb2RhLWRlc2lnbi9kZXNpZ24wOC5wbmciXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0FBQUEsc0YiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svcGFnZXMvV29yay9BZ29kYS1kZXNpZ24uMjczNzliNmUzZGYwMzk0OGM4YmYuaG90LXVwZGF0ZS5qcyIsInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0gXCIvX25leHQvc3RhdGljL2ltYWdlcy9kZXNpZ24wOC1lNDE5MjI4ODhiYTdiOTI5MDNmNGYyYmI1NTFiZGVhMy5wbmdcIjsiXSwic291cmNlUm9vdCI6IiJ9
